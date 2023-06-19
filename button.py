8d6150bd311d7c070c60d9f913a91ac6738bc152
muhammad023 <134907550+muhammad023@users.noreply.github.com>
2023-06-19 12:06:10 +0500

    Add files via upload

---
+++ button.py
@@ -0,0 +1,186 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+bosh_menu = ReplyKeyboardMarkup(
+    keyboard=
+    [
+        [
+            KeyboardButton(text="Yunusobod tumani"),
+          
+        ],
+        [
+            KeyboardButton(text="Shayxontoxur tumani"),
+            KeyboardButton(text="Chilonzor tumani"),
+        
+        ],
+        [
+            KeyboardButton(text="Mirzo Ulugbek tumani"),
+            KeyboardButton(text="Olmazor tumani"),
+        ],
+        [
+            KeyboardButton(text="⬅Ortga")
+        ]
+],
+resize_keyboard=True
+)
+
+Yunusobod_hotels_keyboard = ReplyKeyboardMarkup(
+    keyboard=
+    [
+        [
+            KeyboardButton(text="the elements hotel"),
+            KeyboardButton(text="Sano hotel"),
+
+          
+        ],
+        [
+            KeyboardButton(text="Garnet hotel"),
+            KeyboardButton(text="Khan orda hotel"),
+             KeyboardButton(text="VELARA hotel"),
+        
+        ],
+        
+        [
+              KeyboardButton(text="⬅Ortga")
+            
+        
+        ], 
+
+    ],
+resize_keyboard=True
+)
+
+Chilonzo_hotels_keyboard = ReplyKeyboardMarkup(
+    keyboard=
+    [
+        [
+            KeyboardButton(text="Grand Nur hotel"),
+            KeyboardButton(text="Asil Makon hotel"),
+            KeyboardButton(text="GREENWAY hotel"),
+
+
+          
+        ],
+        [
+            KeyboardButton(text="The Royal Mezbon Hotel and SPA"),
+            KeyboardButton(text="Bolo xona hotel"),
+             KeyboardButton(text="Hostel Point hotel"),
+        
+        ],
+        
+        [
+              KeyboardButton(text="⬅Ortga")
+            
+        
+        ], 
+
+    ],
+resize_keyboard=True
+)
+
+Olmazor_hotels_keyboard = ReplyKeyboardMarkup(
+    keyboard=
+    [
+        [
+            KeyboardButton(text="Sebzor hotel"),
+            KeyboardButton(text="Taht hotel"),
+            KeyboardButton(text="Hostel point hotel"),
+
+
+          
+        ],
+        [
+            KeyboardButton(text="ok hotel"),
+            KeyboardButton(text="Taxptapul hotel"),
+             KeyboardButton(text="Grand Way hotel"),
+        
+        ],
+        
+        [
+              KeyboardButton(text="⬅Ortga")
+            
+        
+        ], 
+
+    ],
+resize_keyboard=True
+)
+MirzoUlugbek_hotels_keyboard = ReplyKeyboardMarkup(
+    keyboard=
+    [
+        [
+            KeyboardButton(text="Darhan Boutique Hotel"),
+            KeyboardButton(text="SHAKH SULTON Hotel"),
+
+          
+        ],
+        [
+            KeyboardButton(text="International Hotel"),
+            KeyboardButton(text="Eurolux  Boutique Hotel"),
+             KeyboardButton(text="Paradise hotel"),
+              KeyboardButton(text="Friday hotel"),
+        
+        ],
+        
+        [
+              KeyboardButton(text="⬅Ortga")
+            
+        
+        ], 
+
+    ],
+resize_keyboard=True
+)
+
+MirzoUlugbek_hotels_keyboard = ReplyKeyboardMarkup(
+    keyboard=
+    [
+        [
+            KeyboardButton(text="Darhan Boutique Hotel"),
+            KeyboardButton(text="SHAKH SULTON Hotel"),
+
+          
+        ],
+        [
+            KeyboardButton(text="International Hotel"),
+            KeyboardButton(text="Eurolux  Boutique Hotel"),
+             KeyboardButton(text="Paradise hotel"),
+              KeyboardButton(text="Friday hotel"),
+        
+        ],
+        
+        [
+              KeyboardButton(text="⬅Ortga")
+            
+        
+        ], 
+
+    ],
+resize_keyboard=True
+)
+
+shayxontoxur_hotels_keyboard = ReplyKeyboardMarkup(
+    keyboard=
+    [
+        [
+            KeyboardButton(text="Лотте Сити Хотел Ташкент Палас"),
+            KeyboardButton(text="Huvaydo Hotel"),
+
+          
+        ],
+        [
+            KeyboardButton(text="Чорсу Инн отель"),
+            KeyboardButton(text="Hotel Suzuk-Ota"),
+             KeyboardButton(text="Mirzo Boutique Hotel"),
+              KeyboardButton(text="Old City Hotel Tashkent"),
+        
+        ],
+        
+        [
+              KeyboardButton(text="⬅Ortga")
+            
+        
+        ], 
+
+    ],
+resize_keyboard=True
+)
